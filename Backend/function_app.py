049906d89f007165b9b2aed33802841ab42d18fe
Rosen Marinov <rosen1710@abv.bg>
2024-03-15 12:33:31 +0200

    New create_hobby method

--- Backend/function_app.py
+++ Backend/function_app.py
@@ -20,87 +20,163 @@ def create_tables():
         conn = create_connection()
         cur = conn.cursor()
 
-        user_table = """
+        create_table = """
             CREATE TABLE IF NOT EXISTS hhuser (
                 id SERIAL PRIMARY KEY,
-                email TEXT NOT NULL,
+                email TEXT NOT NULL UNIQUE,
                 password_hash TEXT NOT NULL,
                 fullname TEXT NOT NULL,
-                age INTEGER NOT NULL,
+                age INT NOT NULL,
                 description TEXT NOT NULL
             );
         """
+        cur.execute(create_table)
 
-        hobby_table = """
+        create_table = """
             CREATE TABLE IF NOT EXISTS hobby (
                 id SERIAL PRIMARY KEY,
-                name TEXT NOT NULL,
+                name TEXT NOT NULL UNIQUE,
                 created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                 approved BOOLEAN NOT NULL
             );
         """
+        cur.execute(create_table)
 
-        cur.execute(user_table)
-        cur.execute(hobby_table)
+        create_table = """
+            CREATE TABLE IF NOT EXISTS hobby_user (
+                id SERIAL PRIMARY KEY,
+                user_id INT NOT NULL REFERENCES hhuser(id),
+                hobby_id INT NOT NULL REFERENCES hobby(id)
+            );
+        """
+        cur.execute(create_table)
+
+        create_table = """
+            CREATE TABLE IF NOT EXISTS channel (
+                id SERIAL PRIMARY KEY,
+                name TEXT NOT NULL UNIQUE,
+                hobby_id INT NOT NULL REFERENCES hobby(id),
+                created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+            );
+        """
+        cur.execute(create_table)
+
+        create_table = """
+            CREATE TABLE IF NOT EXISTS message (
+                id SERIAL PRIMARY KEY,
+                text TEXT NOT NULL,
+                user_id INT REFERENCES hhuser(id),
+                channel_id INT NOT NULL REFERENCES channel(id),
+                created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+            );
+        """
+        cur.execute(create_table)
 
         conn.commit()
         conn.close()
-        print('Tables created or already exist')
     except:
-        print('Error occurred while creating or accessing tables')
-        exit()
+        raise Exception("Error occurred while creating or accessing tables")
 
 def add_user(email, password, fullname, age, description):
     conn = create_connection()
     cur = conn.cursor()
 
-    # password = 
-
-    cur.execute("INSERT INTO hhuser (email, password_hash, fullname, age, description) VALUES (%s, %s, %s, %s, %s) RETURNING id;",
-                (email, bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()), fullname, age, description))
-    user_id = cur.fetchone()[0]
+    try:
+        cur.execute("INSERT INTO hhuser (email, password_hash, fullname, age, description) VALUES (%s, %s, %s, %s, %s) RETURNING id;",
+            (email, bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()), fullname, age, description))
+    except psycopg2.errors.UniqueViolation:
+        conn.commit()
+        conn.close()
+        raise psycopg2.errors.UniqueViolation("User with this email already exist!")
+    except:
+        conn.commit()
+        conn.close()
+        raise Exception("Unhandled exception occurred!")
 
     conn.commit()
     conn.close()
 
-    return func.HttpResponse(
-             "User added successfully",
-             status_code=200
-        )
+def add_hobby(name):
+    conn = create_connection()
+    cur = conn.cursor()
+
+    try:
+        cur.execute("INSERT INTO hobby (name, approved) VALUES (%s, %s) RETURNING id;",
+            (name, False))
+    except psycopg2.errors.UniqueViolation:
+        conn.commit()
+        conn.close()
+        raise psycopg2.errors.UniqueViolation("This hobby already exist!")
+    except:
+        conn.commit()
+        conn.close()
+        raise Exception("Unhandled exception occurred!")
+
+    conn.commit()
+    conn.close()
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="create_user")
 def create_user(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP trigger called create_user function')
 
-    # try:
-    #     create_tables()
-    #     req_body = req.get_json()
-    # except ValueError:
-    #     pass
-    # else:
-    #     try:
-    #         email = req_body.get('email')
-    #         password = req_body.get('password')
-    #         fullname = req_body.get('fullname')
-    #         age = req_body.get('age')
-    #         description = req_body.get('description')
-    #     except:
-    #         return func.HttpResponse(
-    #          "Not valid data provided!",
-    #          status_code=400
-    #     )
+    try:
+        # create_tables()
+        req_body = req.get_json()
+    except ValueError:
+        pass
+    else:
+        try:
+            email = req_body.get('email')
+            password = req_body.get('password')
+            fullname = req_body.get('fullname')
+            age = req_body.get('age')
+            description = req_body.get('description')
+        except Exception as e:
+            return func.HttpResponse(
+                str(e),
+                status_code=400
+            )
 
-    if True: # email and password and fullname and age and description:
-        # add_user(email, password, fullname, age, description)
-        add_user("email", "password", "fullname", 17, "description")
+    try:
+        add_user(email, password, fullname, age, description)
         return func.HttpResponse(
             "User added successfully.",
             status_code=200
         )
-    else:
+    except Exception as e:
         return func.HttpResponse(
-             "Not valid data provided!",
-             status_code=400
+            str(e),
+            status_code=400
+        )
+
+@app.route(route="create_hobby")
+def create_hobby(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger called create_hobby function')
+
+    try:
+        # create_tables()
+        req_body = req.get_json()
+    except ValueError:
+        pass
+    else:
+        try:
+            name = req_body.get('name')
+        except Exception as e:
+            return func.HttpResponse(
+                str(e),
+                status_code=400
+            )
+
+    try:
+        add_hobby(name)
+        return func.HttpResponse(
+            "Hobby added successfully.",
+            status_code=200
+        )
+    except Exception as e:
+        return func.HttpResponse(
+            str(e),
+            status_code=400
         )
