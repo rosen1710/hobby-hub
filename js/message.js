e7c8d41dda54cef2ee43001ce5a27a9578194eef
Rosen Marinov <rosen1710@abv.bg>
2024-03-16 02:32:08 +0200

    Messages sending to db added + improved fetching

--- js/message.js
+++ js/message.js
@@ -1,9 +1,18 @@
+myUserId = 52;
+myName = "You";
+
+otherSenderName = "Ivan";
+
 function loadMessages(sectionId){
-    let section=document.getElementById(sectionId);
-    section.innerHTML="";
-    let messageHtml =`<div class="container lg-6 pt-2" id="${sectionId}-message-container">`;
-    //load data
-    
+
+    fetchMessages(sectionId);
+
+    setInterval(() => {
+        fetchMessages(sectionId)
+    }, 2000);
+}
+
+function fetchMessages(sectionId) {
     fetch('http://localhost:5000/fetch_messages', {
         method: 'POST',
         headers: {
@@ -17,29 +26,35 @@ function loadMessages(sectionId){
     .then(data => {
         //console.log(data);
         console.log(data.response);
+
+        section = document.getElementById(sectionId);
+
+        try {
+            prev_value = document.getElementById(`${sectionId}-message`).value;
+        } catch (error) {
+            prev_value = "";
+        }
+        section.innerHTML="";
+        let messageHtml =`<div class="container lg-6 pt-2" id="${sectionId}-message-container">`;
+
         data.response.forEach(row => {
-            // rows += `<tr>${headers.map(header => `<td>${row[header]}</td>`).join('')}</tr>`;
-            // console.log(row);
-            let fullName="Ivan",
+            let fullName = otherSenderName,
             message = row[1],
             createdAt=row[4].split(".")[0];
             let isMine = false;
+            if(row[2] == myUserId) {
+                isMine = true;
+                fullName = myName;
+            }
             messageHtml="";
             messageHtml += prepareMessageHtml(fullName, message, createdAt, isMine);
             messageHtml +='</div>';
             section.innerHTML+=messageHtml;
         });
         section.innerHTML+=addMessageForm(sectionId);
-    })
-
-    // let fullName="ivan",
-    // message = "moje",
-    // createdAt="11/11/2015";
-    // let isMine = false;
-    // messageHtml += prepareMessageHtml(fullName, message, createdAt, isMine);
-    // messageHtml +='</div>';
-    // section.innerHTML+=messageHtml;
-    // section.innerHTML+=addMessageForm(sectionId);
+        document.getElementById(`${sectionId}-message`).value = prev_value;
+        document.getElementById(`${sectionId}-message`).focus();
+    });
 }
 
 function prepareMessageHtml(fullName, message, createdAt, isMine) {
@@ -54,13 +69,11 @@ function prepareMessageHtml(fullName, message, createdAt, isMine) {
         </div>`;
 }
 
-
-
 function addMessageForm(sectionId){
     return `<div name="${sectionId}-form">
         <div class="row pb-2 pt-5">
             <div class="col-9 col-lg-10">
-                <input name="${sectionId}-message" type="text" id="${sectionId}-message" class="container-fluid form-control">
+                <input name="${sectionId}-message" type="text" id="${sectionId}-message" class="container-fluid form-control" autofocus>
             </div>
             <div class="col-2 col-lg-1">
                 <button type="button" id="send-btn" class="col container-fluid form-control" onclick="insertMessage('${sectionId}')">Send</button>
@@ -70,10 +83,8 @@ function addMessageForm(sectionId){
 }
 
 function insertMessage(sectionId){
-    // let messageContainer=document.getElementById(sectionId+"-message-container");
     let messageContainer;
     let distance = 0;
-    // =document.getElementsByClassName("displayed-message");
     collection = document.getElementsByClassName("displayed-message");
     for (let i = 0; i < collection.length; i++) {
         if(collection[i].getBoundingClientRect().top>distance){
@@ -86,7 +97,17 @@ function insertMessage(sectionId){
     messageInput.value="";
     let date = new Date().toJSON().split(".")[0];
     date = date.split("T")[0] + " " + date.split("T")[1];
-    messageContainer.innerHTML += prepareMessageHtml("You", newMessage, date, true);
+    messageContainer.innerHTML += prepareMessageHtml(myName, newMessage, date, true);
 
+    fetch('http://localhost:5000/create_message', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            text: newMessage,
+            user_id: myUserId,
+            channel_id: 23
+        })
+    });
 }
-
